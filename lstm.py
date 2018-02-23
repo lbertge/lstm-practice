60df165f6983eee9463ff3671982a3b638019f67
Albert Ge <lbertge@gmail.com>
2018-02-23 01:56:26 -0800

    LSTMs working, but I don't understand why accuracy is so bad

--- lstm.py
+++ lstm.py
@@ -16,52 +16,48 @@ import numpy as np
     explains the difference?
 """
 
-TOTAL_SIZE = 100
-TRAINING_SPLIT = 0.64
-VALIDATION_SPLIT = 0.16
-TEST_SPLIT = 0.2
+TOTAL_SIZE = 10
+DATA_SIZE = int(0.6 * TOTAL_SIZE)
+VAL_SIZE = int(0.2 * TOTAL_SIZE)
+TEST_SIZE = int(0.2 * TOTAL_SIZE)
+
 BATCH_SIZE = 1
 
-LENGTH = 50
+LENGTH = 2
 NUM_INPUT = 1
-NUM_UNITS = 512
+NUM_UNITS = 128
 NUM_CLASSES = 2
 RATE = 0.001
-EPOCH = 10
+EPOCH = 2000
+PRINT_EVERY_EPOCH = EPOCH / 10
 
+STOPPING_CRIT = 0.05
 
 # Creating random sequences and labels
-train = tf.floor(tf.random_uniform([int(TRAINING_SPLIT * TOTAL_SIZE), LENGTH]) * 2)
-train_label = tf.mod(tf.reduce_sum(train, 1), tf.constant(2.0))
-train_label = tf.map_fn(lambda x : [1., 0.] if x > 0 else [0., 1.], train_label)
+one_hot = lambda x: [0., 1.] if x == 1 else [1., 0.]
+f = np.vectorize(one_hot)
 
-valid = tf.floor(tf.random_uniform([int(VALIDATION_SPLIT * TOTAL_SIZE), LENGTH]) * 2)
-valid_label = tf.mod(tf.reduce_sum(valid, 1), tf.constant(2.0))
-valid_label = tf.map_fn(lambda x : [1., 0.] if x > 0 else [0., 1.], valid_label)
+train_X = np.floor(np.random.rand(DATA_SIZE, LENGTH) * 2)
+train_X = train_X.reshape(DATA_SIZE, LENGTH, 1)
+train_Y = np.array(np.mod(np.sum(train_X, 1), 2.0))
+train_Y = np.squeeze(train_Y)
+train_logits = np.zeros((len(train_Y), NUM_CLASSES))
+train_logits[np.arange(train_Y.size), train_Y.astype('int64')] = 1
 
-test = tf.floor(tf.random_uniform([int(TEST_SPLIT * TOTAL_SIZE), LENGTH]) * 2)
-test_label = tf.mod(tf.reduce_sum(valid, 1), tf.constant(2.0))
-test_label = tf.map_fn(lambda x : [1., 0.] if x > 0 else [0., 1.], test_label)
 
-train_dataset = tf.data.Dataset.from_tensor_slices((train, train_label))
-valid_dataset = tf.data.Dataset.from_tensor_slices((valid, valid_label))
-test_dataset = tf.data.Dataset.from_tensor_slices((test, test_label))
+valid_X = np.floor(np.random.rand(VAL_SIZE, LENGTH) * 2)
+valid_X = valid_X.reshape(VAL_SIZE, LENGTH, 1)
+valid_Y = np.array(np.mod(np.sum(valid_X, 1), 2.0))
+valid_Y = np.squeeze(valid_Y)
+valid_logits = np.zeros((len(valid_Y), NUM_CLASSES))
+valid_logits[np.arange(valid_Y.size), valid_Y.astype('int64')] = 1
 
-#Create an iterator for accessing elements in batches
-train_batch = train_dataset.batch(BATCH_SIZE)
-train_batch = train_batch.repeat(EPOCH)
-
-valid_batch = valid_dataset.batch(BATCH_SIZE)
-valid_batch = vaild_batch.repeat(EPOCH)
-
-train_it = train_dataset.make_one_shot_iterator()
-train_next = train_it.get_next()
-
-valid_it = valid_dataset.make_one_shot_iterator()
-valid_next = valid_it.get_next()
-
-test_it = test_dataset.make_one_shot_iterator()
-test_next = test_it.get_next()
+test_X = np.floor(np.random.rand(TEST_SIZE, LENGTH) * 2)
+test_X = test_X.reshape(TEST_SIZE, LENGTH, 1)
+test_Y = np.array(np.mod(np.sum(test_X, 1), 2.0))
+test_Y = np.squeeze(test_Y)
+test_logits = np.zeros((len(test_Y), NUM_CLASSES))
+test_logits[np.arange(test_Y.size), test_Y.astype('int64')] = 1
 
 # build LSTM
 weights = {
@@ -71,22 +67,22 @@ biases = {
     'out': tf.Variable(tf.random_normal([NUM_CLASSES]))
 }
 
-X = tf.placeholder("float", [None, LENGTH, N_INPUT])
+X = tf.placeholder("float", [None, LENGTH, NUM_INPUT])
 Y = tf.placeholder("float", [None, NUM_CLASSES])
 
 def network(x, weights, biases):
-    inp = tf.unstack(x, LENGTH, axis=1)
-    network = tf.contrib.rnn.BasicLSTMCell(NUM_UNITS)
-    output, states = rnn.static_rnn(network, inp, dtype=tf.float32)
+    x = tf.unstack(x, LENGTH, axis=1)
+    lstm = tf.contrib.rnn.BasicLSTMCell(NUM_UNITS)
+    output, states = tf.contrib.rnn.static_rnn(lstm, x, dtype=tf.float32)
 
-    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+    return tf.matmul(output[-1], weights['out']) + biases['out']
 
-logits = RNN(X, weights, biases)
+logits = network(X, weights, biases)
 pred = tf.nn.softmax(logits)
 
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
-opt = tf.train.GradientDescentOptimizer(learning_rate = RATE)
-train = op.minimize(loss)
+opt = tf.train.AdamOptimizer(learning_rate = RATE)
+train = opt.minimize(loss)
 
 correct = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
 acc = tf.reduce_mean(tf.cast(correct, tf.float32))
@@ -98,15 +94,22 @@ with tf.Session() as sess:
     sess.run(init)
 
     for step in range(EPOCH):
-        batch_x, batch_y = train_next
-        sess.run(train, feed_dict={X: batch_x, Y: batch_y})
-        if step % 100 == 0:
-            l, a = sess.run([loss, acc], feed_dict={X:batch_x, Y: batch_y})
-            print("Step" + str(step) + ", Loss= " + \
-                    "{:.3f}".format(l) + ", Acc= " + \
+        train_loss = sess.run(train, feed_dict={X: train_X, Y: train_logits})
+
+        a = None
+        if step % PRINT_EVERY_EPOCH == 0:
+            l, a = sess.run([loss, acc], feed_dict={X: valid_X, Y: valid_logits})
+            print("Step " + str(step) + ", Valid Loss= " + \
+                    "{:.3f}".format(l) + ", Valid Acc= " + \
                     "{:.3f}".format(a))
-    
-    print("Test Acc:", sess.run(acc, feed_dict={X: test, Y: test_label}))
+        if a and 1 - a < STOPPING_CRIT:
+            print("Epoch " + str(step) + "; stopping here, I have probably memorized the set")
+            break
 
+    print("Test Acc:", sess.run(acc, feed_dict={X: test_X, Y: test_logits}))
+    print("Train Acc:", sess.run(acc, feed_dict={X: train_X, Y: train_logits}))
+    print("Valid Acc:", sess.run(acc, feed_dict={X: valid_X, Y: valid_logits}))
 
-
+print(valid_X.T)
+print(train_X.T)
+print(test_X.T)
